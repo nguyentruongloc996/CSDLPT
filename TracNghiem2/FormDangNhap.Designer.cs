1804956903efc0d81eeff4b7eae1b78b7f5b4324
Loc <nguyentruongloc996@gmail.com>
2018-08-12 17:42:26 +0700

    8/12

---
+++ TracNghiem2/FormDangNhap.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TracNghiem2
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label tENCNLabel;
+            this.tRACNGHIEMDataSet = new TracNghiem2.TRACNGHIEMDataSet();
+            this.cOSOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cOSOTableAdapter = new TracNghiem2.TRACNGHIEMDataSetTableAdapters.COSOTableAdapter();
+            this.tableAdapterManager = new TracNghiem2.TRACNGHIEMDataSetTableAdapters.TableAdapterManager();
+            this.bdsDSPM = new System.Windows.Forms.BindingSource(this.components);
+            this.v_DS_PHANMANHTableAdapter = new TracNghiem2.TRACNGHIEMDataSetTableAdapters.V_DS_PHANMANHTableAdapter();
+            this.tENCNComboBox = new System.Windows.Forms.ComboBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            tENCNLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tRACNGHIEMDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cOSOBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsDSPM)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tENCNLabel
+            // 
+            tENCNLabel.AutoSize = true;
+            tENCNLabel.Location = new System.Drawing.Point(39, 60);
+            tENCNLabel.Name = "tENCNLabel";
+            tENCNLabel.Size = new System.Drawing.Size(63, 13);
+            tENCNLabel.TabIndex = 0;
+            tENCNLabel.Text = "Chi Nhánh :";
+            tENCNLabel.Click += new System.EventHandler(this.tENCNLabel_Click);
+            // 
+            // tRACNGHIEMDataSet
+            // 
+            this.tRACNGHIEMDataSet.DataSetName = "TRACNGHIEMDataSet";
+            this.tRACNGHIEMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cOSOBindingSource
+            // 
+            this.cOSOBindingSource.DataMember = "COSO";
+            this.cOSOBindingSource.DataSource = this.tRACNGHIEMDataSet;
+            this.cOSOBindingSource.CurrentChanged += new System.EventHandler(this.cOSOBindingSource_CurrentChanged);
+            // 
+            // cOSOTableAdapter
+            // 
+            this.cOSOTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.BANGDIEMTableAdapter = null;
+            this.tableAdapterManager.BODETableAdapter = null;
+            this.tableAdapterManager.COSOTableAdapter = this.cOSOTableAdapter;
+            this.tableAdapterManager.GIAOVIEN_DANGKYTableAdapter = null;
+            this.tableAdapterManager.GIAOVIENTableAdapter = null;
+            this.tableAdapterManager.KHOATableAdapter = null;
+            this.tableAdapterManager.LOPTableAdapter = null;
+            this.tableAdapterManager.MONHOCTableAdapter = null;
+            this.tableAdapterManager.SINHVIENTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = TracNghiem2.TRACNGHIEMDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // bdsDSPM
+            // 
+            this.bdsDSPM.DataMember = "V_DS_PHANMANH";
+            this.bdsDSPM.DataSource = this.tRACNGHIEMDataSet;
+            // 
+            // v_DS_PHANMANHTableAdapter
+            // 
+            this.v_DS_PHANMANHTableAdapter.ClearBeforeFill = true;
+            // 
+            // tENCNComboBox
+            // 
+            this.tENCNComboBox.DataSource = this.bdsDSPM;
+            this.tENCNComboBox.DisplayMember = "TENCN";
+            this.tENCNComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tENCNComboBox.FormattingEnabled = true;
+            this.tENCNComboBox.Location = new System.Drawing.Point(151, 57);
+            this.tENCNComboBox.Name = "tENCNComboBox";
+            this.tENCNComboBox.Size = new System.Drawing.Size(279, 21);
+            this.tENCNComboBox.TabIndex = 1;
+            this.tENCNComboBox.ValueMember = "TENSERVER";
+            this.tENCNComboBox.SelectedIndexChanged += new System.EventHandler(this.tENCNComboBox_SelectedIndexChanged);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(151, 84);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(279, 21);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDangNhap);
+            this.groupBox1.Controls.Add(this.btnThoat);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(tENCNLabel);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Controls.Add(this.tENCNComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(67, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 261);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(299, 169);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(151, 112);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(279, 21);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(151, 168);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 8;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 362);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormDangNhap";
+            this.Text = "FromDangNhap";
+            this.Load += new System.EventHandler(this.FromDangNhap_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tRACNGHIEMDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cOSOBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsDSPM)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TRACNGHIEMDataSet tRACNGHIEMDataSet;
+        private System.Windows.Forms.BindingSource cOSOBindingSource;
+        private TRACNGHIEMDataSetTableAdapters.COSOTableAdapter cOSOTableAdapter;
+        private TRACNGHIEMDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingSource bdsDSPM;
+        private TRACNGHIEMDataSetTableAdapters.V_DS_PHANMANHTableAdapter v_DS_PHANMANHTableAdapter;
+        private System.Windows.Forms.ComboBox tENCNComboBox;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
---
+++ TracNghiem2/FormDangNhap.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace TracNghiem2
+{
+    public partial class FormDangNhap : DevExpress.XtraEditors.XtraForm
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void cOSOBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.cOSOBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.tRACNGHIEMDataSet);
+
+        }
+
+        private void FromDangNhap_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tRACNGHIEMDataSet.V_DS_PHANMANH' table. You can move, or remove it, as needed.
+            this.v_DS_PHANMANHTableAdapter.Fill(this.tRACNGHIEMDataSet.V_DS_PHANMANH);
+            // TODO: This line of code loads data into the 'tRACNGHIEMDataSet.COSO' table. You can move, or remove it, as needed.
+            this.cOSOTableAdapter.Fill(this.tRACNGHIEMDataSet.COSO);
+            tENCNComboBox.SelectedIndex = 1;
+            tENCNComboBox.SelectedIndex = 0;
+        }
+
+        private void cOSOBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tENCNLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void tENCNComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Program.servername = tENCNComboBox.SelectedText.ToString();
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            FormDangNhap.ActiveForm.Close();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text.Trim() == "")
+            {
+                MessageBox.Show("Tài khoản đăng nhập không được rỗng", "Báo lỗi đăng nhập", MessageBoxButtons.OK);
+                txtUserName.Focus();
+                return;
+            }
+            Program.mlogin = txtUserName.Text;
+            Program.password = txtPassword.Text;
+            if (Program.KetNoi() == 0)
+            {
+                return;
+            }
+            MessageBox.Show("Đăng nhập thành công", "Đăng nhập", MessageBoxButtons.OK);
+        }
+    }
+}
